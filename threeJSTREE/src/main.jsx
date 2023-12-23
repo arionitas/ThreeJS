f127e48d3ffc8f2510523679b3a457aeb9297c93
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2023-12-23 17:00:52 +0000

    set up the scene and added sphere geometry, with it's material and mesh parameters to the scene

--- threeJSTREE/src/main.jsx
+++ threeJSTREE/src/main.jsx
@@ -1,2 +1,21 @@
-import * as THREE from 'three';
+import * as THREE from 'three'; //Importing three library into project file
+
+// SCENE
+
+const scene = new THREE.Scene(); //Creating Scene
+
+//SPHERE CREATION
+
+const geometry = new THREE.SphereGeometry(3, 64, 64); //Creating sphere with parameters that define it's size and curves
+const material = new THREE.MeshStandardMaterial({ // Defining the mesh material settings
+  color: "0x00ff00",
+  roughness: 0.5,
+  metalness: 0.5
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh) //adding mesh to scene
+
+// CAMERA
+
 
