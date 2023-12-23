e2cd623e2ddbd74d1c3c2f84350b911400716479
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2023-12-23 17:08:14 +0000

    scene now successfully renders a black square after finding the bug in code where 'THREE' was written as 'Three'

--- threeJSTREE/src/main.jsx
+++ threeJSTREE/src/main.jsx
@@ -8,9 +8,7 @@ const scene = new THREE.Scene(); //Creating Scene
 
 const geometry = new THREE.SphereGeometry(3, 64, 64); //Creating sphere with parameters that define it's size and curves
 const material = new THREE.MeshStandardMaterial({ // Defining the mesh material settings
-  color: "0x00ff00",
-  roughness: 0.5,
-  metalness: 0.5
+  color: "#ff0000",
 });
 
 const mesh = new THREE.Mesh(geometry, material);
@@ -18,9 +16,14 @@ scene.add(mesh) //adding mesh to scene
 
 // CAMERA - What we see on the screen
 
-const camera = new Three.PerspectiveCamera(45, 800, 600)
+const camera = new THREE.PerspectiveCamera(45, 800, 600)
+camera.position.z = 20
 scene.add(camera);
 
 
+// RENDERING 
 
-
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
