2cfed5dc67ac9cd07a23f9e9a41222671bea8ce0
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2023-12-23 17:20:43 +0000

    successfully rendered 3D sphere geometry, fixed lighting bug - had to amplify the intensity to see it

--- threeJSTREE/src/main.jsx
+++ threeJSTREE/src/main.jsx
@@ -8,16 +8,24 @@ const scene = new THREE.Scene(); //Creating Scene
 
 const geometry = new THREE.SphereGeometry(3, 64, 64); //Creating sphere with parameters that define it's size and curves
 const material = new THREE.MeshStandardMaterial({ // Defining the mesh material settings
-  color: "#ff0000",
+  color: "#00ff83",
 });
+
 
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh) //adding mesh to scene
 
+// LIGHTING - adding lights to the scene
+
+const light = new THREE.PointLight(0xFFFFFF, 70, 100, 1.7);
+light.position.set(0, 10, 10)
+scene.add(light);
+
+
 // CAMERA - What we see on the screen
 
-const camera = new THREE.PerspectiveCamera(45, 800, 600)
-camera.position.z = 20
+const camera = new THREE.PerspectiveCamera(45, 800/600);
+camera.position.z = 20 // Moving the camera in Z-space back by 20 meters
 scene.add(camera);
 
 
@@ -28,9 +36,6 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(800, 600);
 renderer.render(scene, camera);
 
+console.log(renderer)
 
-// LIGHTING - adding lights to the scene
 
-const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(0, 10, 10)
-scene.add(light);
